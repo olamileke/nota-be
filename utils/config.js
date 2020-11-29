dbba7e2863717894c3fbc5e8b4f72458eca2906c
Fambegbe Olamileke <olamileke.f@gmail.com>
2020-11-29 23:26:58 +0100

    scrubbed configuration of app secrets

---
+++ utils/config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+exports.clientUrl = 'http://localhost:3000/';
+
+exports.mongodbConnectionString = '';
+
+exports.awsAccessKeyId = '';
+
+exports.awsSecretKey = '';
+
+exports.awsRegion = '';
+
+exports.awsBucketName = '';
+
+exports.s3FileLink = '';
+
+exports.secretKey = 'mYYP2dJBNeyclufoYuEJkjhnkljkjsdfhkjdlfk';
+
+exports.activityPerPage = 7;
+
+exports.perPage = 6;
+
+exports.mailApiKey = '';
+
+exports.mailDomain = '';
+
+exports.mail = exports.mail = {
+    from:'Nota <users@nota.in>',
+    to:'',
+    subject:'',
+    html:'',
+    inline:path.join('templates', 'icon.png')
+}; 
